f5c644262842f9ff1437568725ea45724ca9a207
Allochka11 <allavelychko11@gmail.com>
2022-07-27 20:26:53 +0300

    5 lesson Two buttons with different colours another variant

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classes from './OnOff.module.css';
 type OnOffType = {
     on: boolean
     off:boolean
@@ -9,9 +10,12 @@ const OnOff = (props:OnOffType) => {
 
     return (
         <div>
-            {props.on ? <button style={{backgroundColor:"green"}}>on</button>  : <button >on</button>}
-            {props.off ? <button style={{backgroundColor:"red"}}>off</button> : <button >off</button>}
-            {props.on ? <button style={{backgroundColor:"green", borderRadius:"50%"}}>o</button> : <button style={{backgroundColor:"red",borderRadius:"50%"}}>o</button>}
+            <button className={`${props.on && classes.green}`}>on</button>
+            <button className={`${props.off && classes.red }`}>off</button>
+            <button className={`${classes.radius} ${props.on ? classes.green : classes.red}`}>o</button>
+            {/*{props.on ? <button style={{backgroundColor:"green"}}>on</button>  : <button >on</button>}*/}
+            {/*{props.off ? <button style={{backgroundColor:"red"}}>off</button> : <button >off</button>}*/}
+            {/*{props.on ? <button style={{backgroundColor:"green", borderRadius:"50%"}}>o</button> : <button style={{backgroundColor:"red",borderRadius:"50%"}}>o</button>}*/}
         </div>
     )
 }
